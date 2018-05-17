ed26f964858d5d860a66e06a836c4b654ba64648
jennifer123 <jenifferamezquita@ccpaedu.com>
2018-05-17 21:43:24 +0000

    Updating my site

---
+++ script.js
@@ -0,0 +1,14 @@
+$("document").ready (function() {
+    $(".nav").hide(); 
+    
+    $(".menu").click(function() {
+        $(".nav").slideDown();
+    });
+    
+    4(".content").click(function(){
+        $(".nav").slideUp();
+    });
+    
+    
+    
+});
